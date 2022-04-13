17a104a558438e7186df0617648e8c7b56d720b6
Jack Norris <jacktimothynorris@gmail.com>
2022-04-13 00:50:11 -0500

    Blank endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ import configureEndpoints from './api';
 import express_app from './loaders/express';
 const port = 3000;
 
-configureEndpoints(express_app);
+//configureEndpoints(express_app);
+express_app.get('/', (req, res) => {
+  res.send("HELLO WORLD");
+});
 
 express_app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
