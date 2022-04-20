37c1c56bf7df9d53b0024b7cc75d22b2b8317afc
Jack Norris <jacktimothynorris@gmail.com>
2022-04-20 15:39:56 -0500

    updated data modle package

--- src/api/clothingItems.ts
+++ src/api/clothingItems.ts
@@ -1,15 +1,21 @@
 import databaseConnection from "../loaders/postgres";
-import { Request, Response, Express } from 'express';
+import { Express } from 'express';
 import { getClothingItems } from "../services/clothingItemsService";
+import { CreateClothingItemRequest } from "@uark-acm/bho-data-models/lib";
 
 const configureClothingItemEndpoints = (app: Express) => {
     app.get('/clothingItems', async (req, res) => {
        const clothes = await getClothingItems();
        res.send(clothes);
     });
+
+    app.post('/clothingItems', (req, res) => {
+        res.send("return the created object here");
+    });
+
     app.get('/', (req, res) => {
         res.send("test endpoint");
-    })
+    });
 }
 
 export default configureClothingItemEndpoints;
--- src/config/postgresConfig.ts
+++
@@ -1,4 +0,0 @@
-export const connectionString = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-};
