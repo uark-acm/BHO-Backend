c7d52939f1c8a66f9d92d7156b0fe706b9ff9b15
Jack Norris <jacktimothynorris@gmail.com>
2022-04-20 08:45:39 -0500

    gitignore update

--- dist/api/orders.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const configureOrderEndpoints = (app) => {
-    console.log("Test");
-};
-exports.default = configureOrderEndpoints;
-//# sourceMappingURL=orders.js.map
--- dist/config/postgresConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectionString = void 0;
-exports.connectionString = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-};
-//# sourceMappingURL=postgresConfig.js.map
