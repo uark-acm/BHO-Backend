0c631b84e6aa98747fa9810d9097036c2d5f00c8
Jack Norris <jacktimothynorris@gmail.com>
2022-04-20 15:42:32 -0500

    boilerplate for order endpoints

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -4,6 +4,10 @@ const configureOrderEndpoints = (app: Express) => {
     app.get('/orders', async (req, res) => {
         res.send([]);
     });
+
+    app.post('/orders', (req, res) => {
+         res.send("return the created object here");
+    });
 }
 
 export default configureOrderEndpoints;
