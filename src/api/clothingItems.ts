c5279417d32eb34d3f3d9c4e144641ee60c8bc0b
Jack Norris <jacktimothynorris@gmail.com>
2022-04-20 01:51:27 -0500

    boilerplate for fetching clothing items

--- src/api/clothingItems.ts
+++ src/api/clothingItems.ts
@@ -1,13 +1,14 @@
 import databaseConnection from "../loaders/postgres";
 import { Request, Response, Express } from 'express';
+import { getClothingItems } from "../services/clothingItemsService";
 
 const configureClothingItemEndpoints = (app: Express) => {
-    app.get('/clothingItems', async (req: Request, res: Response) => {
-        const response = await databaseConnection.query('select * from orders;')
-        res.send('Hello world!: ' + JSON.stringify((await response).rows));
+    app.get('/clothingItems', async (req, res) => {
+       const clothes = await getClothingItems();
+       res.send(clothes);
     });
     app.get('/', (req, res) => {
-        res.send('hello');
+        res.send("test endpoint");
     })
 }
 
--- src/services/clothingItemsService.ts
+++ src/services/clothingItemsService.ts
@@ -1,6 +1,11 @@
+import { ClothingItem, ClothingItemDatabaseRow, DatabaseRowToClothingItem } from "@uark-acm/bho-data-models/lib";
 import databaseConnection from "../loaders/postgres"
 
-export const getClothingItems = async () => {
-    const queryResponse = await databaseConnection.query('select * from orders;');
-    return 
+
+export const getClothingItems = async (): Promise<ClothingItem[]> => {
+    const queryResponse = await databaseConnection.query('select * from clothing_items;');
+    const dbClothingItems: ClothingItemDatabaseRow[] = (await queryResponse).rows;
+    return dbClothingItems.map(item => {
+        return DatabaseRowToClothingItem(item)
+    });
 }
