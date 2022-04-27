4b568c8f154226d7a0b58e76a36657463f0712df
Jack Norris <jacktimothynorris@gmail.com>
2022-04-27 02:04:01 -0500

    added cors

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 const express_app = express();
-
+express_app.use(cors());
 express_app.use(bodyParser.urlencoded({extended:false}));
 express_app.use(bodyParser.json())
 
