dfa1e0ea6b930b7295ca0863eba9028db5b9ef6a
Jack Norris <jacktimothynorris@gmail.com>
2022-12-01 03:19:50 -0600

    added sequelize models

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import configureEndpoints from './api';
 import express_app from './loaders/express';
+import * as sqlizer from './models/sequelizeModels'
+import { sequelize, sequelizeConnection } from './loaders/sequelize';
 const port = process.env.PORT || 80;
-
+/*
 configureEndpoints(express_app);
-
-express_app.listen(port, () => {
+*/
+express_app.listen(port, async () => {
+  sqlizer.borrow_request;
+  sqlizer.bho_item;
+  sqlizer.bho_item_category;
+  sqlizer.bho_order;
+  await sequelizeConnection()
+  await sequelize.sync();
+  console.log("All models were synchronized successfully.");
   return console.log(`Express is listening at http://localhost:${port}`);
 });
---
+++ src/loaders/sequelize.ts
@@ -0,0 +1,27 @@
+import { Sequelize } from "sequelize"
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+export const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+})
+
+export const sequelizeConnection = async () => {
+  try {
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log('Postgres connection has been established successfully.')
+      })
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
