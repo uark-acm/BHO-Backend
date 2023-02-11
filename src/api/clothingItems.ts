d32b00defeab49804f49d92cf2dea2b274654c6f
jakilgo <justin.kilgo314@gmail.com>
2023-02-10 18:07:08 -0600

    update attributes

--- src/api/clothingItems.ts
+++ src/api/clothingItems.ts
@@ -10,7 +10,17 @@ const configureClothingItemEndpoints = (app: Express) => {
     });
 
     app.post('/clothingItems', async (req: Request<object, string, BHOItemCreateRequest>, res) => {
-        const clothingItem = await createItem(req.BHOItemCreateRequest)
+        const request = req.body
+        const attributes: BHOItemCreationAttributes = {
+            item_name: request.name,
+            in_stock: true,
+            item_description: request.description,
+            category_id: request.category_id,
+            size: request.size,
+            item_image_url: request.image,
+            set_id: request.set_id,
+        }
+        const clothingItem = await createItem(attributes)
         res.send("return the created object here");
     });
 
