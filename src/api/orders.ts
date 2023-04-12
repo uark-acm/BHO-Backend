f9e440887f5a003e4274724cd2c57bc0a276be9c
Mitchell Bylak <78622329+mjbylak@users.noreply.github.com>
2023-04-11 23:49:00 -0500

    gah

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -5,7 +5,23 @@ const configureOrderEndpoints = (app: Express) => {
         res.send([]);
     });
 
-    app.post('/orders', (req, res) => {
+        /*
+    app.post('/orders', async (req: Request<object, string, BHOOrderReq>, res) => {
+        const { name, description, category_id, size, image, set_id } =
+            req.body;
+        const attributes: BHOItemCreationAttributes = {
+            item_name: name,
+            in_stock: true,
+            item_description: description,
+            category_id: category_id,
+            size: size,
+            item_image_url: image,
+            set_id: set_id,
+        };
+        const clothingItem = await createItem(attributes);
+        res.send('return the created object here');
+    });
+    */
         res.send('return the created object here');
     });
 };
