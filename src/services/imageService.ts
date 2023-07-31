50e53c0dbed1a542626e7d9242ef2f6089b26539
Jack Norris <jacktimothynorris@gmail.com>
2023-07-30 23:55:06 -0500

    added utility function for uploading an image via s3

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,11 +1,15 @@
 import s3 from '../loaders/s3';
+import { PutObjectRequest } from 'aws-sdk/clients/s3';
 
-const bucketName = 'bho-image-bucket';
+const bucketName = 'bho-images-bucket';
 
-export const getImage = async (url: string) => {
-    const params = {
+//typescript function to upload an image to s3 bucket
+export const uploadImage = async (file: File) => {
+    const params: PutObjectRequest = {
         Bucket: bucketName,
-        Key: url,
+        Key: `${Date.now().toString()}.${file.type.replace('image/', '')}`,
+        Body: Buffer.from(await file.arrayBuffer()),
+        ContentType: file.type,
     };
-    return await s3.getObject(params);
+    return (await s3.upload(params).promise()).Key;
 };
