f30be8cf3c224fa2867bdce006ce1dc21e938eae
Jack Norris <jacktimothynorris@gmail.com>
2023-09-19 08:13:31 -0500

    Refactored category service

--- src/services/categoriesService.ts
+++ src/services/categoriesService.ts
@@ -1,5 +1,13 @@
-import BHOItemCategory from '../models/bho_item_category.model';
+import { BHOItemCategory } from '@uark-acm/bho-data-models/lib';
+import BHOItemCategoryDBRow from '../models/bho_item_category.model';
 
 export const getCategories = async (): Promise<BHOItemCategory[]> => {
-    return await BHOItemCategory.findAll();
+    const categories = await BHOItemCategoryDBRow.findAll();
+    return categories.map((category) => {
+        return {
+            id: category.id,
+            name: category.category_name,
+            sizes: category.sizes,
+        };
+    });
 };
